0db21efa7bc944c122462233d33abb4db0f42361
YUNXIU XU <1@1.1>
2025-12-19 02:04:59 +0900

    改了计划书

--- PositionBasedDynamics/Utils/Version.h
+++ PositionBasedDynamics/Utils/Version.h
@@ -5,7 +5,7 @@
 #define STRINGIZE(x) STRINGIZE_HELPER(x)
 #define WARNING(desc) message(__FILE__ "(" STRINGIZE(__LINE__) ") : Warning: " #desc)
 
-#define GIT_SHA1 "4481f08775998cb54a2b3ca6be7495f514cd7455"
+#define GIT_SHA1 "cafb25fc7f951d1d5ac3a2f17fb001de6ae90459"
 #define GIT_REFSPEC "refs/heads/main"
 #define GIT_LOCAL_STATUS "DIRTY"
 
